63fbe1bba2d77bf77f285835fdd606c8c1f60936
poppy california <johannnhandoko@gmail.com>
2025-06-07 21:03:58 +0200

    Remove node_modules and dist from tracking

--- backend/dist/controllers/expenseController.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-export declare const getAllExpenses: (req: Request, res: Response) => Promise<void>;
-export declare const getExpenseById: (req: Request, res: Response) => Promise<void>;
-export declare const createExpense: (req: Request, res: Response) => Promise<void>;
-export declare const updateExpense: (req: Request, res: Response) => Promise<void>;
-export declare const deleteExpense: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=expenseController.d.ts.map
